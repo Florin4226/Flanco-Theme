90b033e205a4d0d7eac9523630e4764f6aa19742
Ayush <77203747+RTK23-dev@users.noreply.github.com>
2021-10-15 16:56:00 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,5 @@
-
+php /var/www/pterodactyl/artisan down
+cd /var/www/pterodactyl
+rm -rf tailwind.config.js
+wget https://raw.githubusercontent.com/Florinacio/Flanco-Theme/main/tailwind.config.js
+cd
